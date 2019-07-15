17195a6f0a393595b9bed9d819c08d73af2abd6e
Oneil Malik <oneilmalik@oneils-macbook-pro.local>
2019-07-15 12:48:13 +0100

    Initial Commit

---
+++ GitIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitIntegration
+//
+//  Created by Oneil Malik on 15/07/2019.
+//  Copyright © 2019 interactivedreamscapes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
